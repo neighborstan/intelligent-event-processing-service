f0ff7cf7b8d5ff69c5609024c2ebf8a9c8eeb9f3
konstantin <mailspace.hub@gmail.com>
2024-06-06 17:53:17 +0600

    Added basic project skeleton

---
+++ src/main/java/art/evalevi/intelligenteventprocessingservice/IntelligentEventProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package art.evalevi.intelligenteventprocessingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentEventProcessingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentEventProcessingServiceApplication.class, args);
+    }
+
+}
